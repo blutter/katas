bc9d80d4a55ea18fca12c7a4cca9e5caba5836e0
Benjamin Tse <benjamin.l.tse@gmail.com>
2016-06-09 23:38:42 +1000

    Add broken reference to fix

--- fizzbuzz/src/1st.spec.ts
+++ fizzbuzz/src/1st.spec.ts
@@ -1,3 +1,9 @@
+/// <reference path="../typings/globals/jasmine/index.d.ts"/>
+
 describe('1st tests', () => {
   it('true is true', () => expect(true).toEqual(true));
+
+  it('null is not the same thing as undefined',
+    () => expect(null).not.toEqual(undefined)
+  );
 });
