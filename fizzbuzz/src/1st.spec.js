72b6a7db7f4ef8c1a3bc2ddb75f77a3e3bd4d7cd
Benjamin Tse <benjamin.l.tse@gmail.com>
2016-06-09 23:26:37 +1000

    Add extra dev dependencies to get "npm start" running

--- fizzbuzz/src/1st.spec.js
+++ fizzbuzz/src/1st.spec.js
@@ -1,2 +1,4 @@
-it('true is true', function () { return expect(true).toEqual(true); });
+describe('1st tests', function () {
+    it('true is true', function () { return expect(true).toEqual(true); });
+});
 //# sourceMappingURL=1st.spec.js.map
--- fizzbuzz/src/1st.spec.ts
+++ fizzbuzz/src/1st.spec.ts
@@ -1,1 +1,3 @@
-it('true is true', () => expect(true).toEqual(true));
+describe('1st tests', () => {
+  it('true is true', () => expect(true).toEqual(true));
+});
