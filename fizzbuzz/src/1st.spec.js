828c23700383fcd6e61d1b97d21a106226ce2868
Benjamin Tse <benjamin.l.tse@gmail.com>
2016-06-09 23:18:40 +1000

    Fix running tsc script (for "npm run tsc")

--- fizzbuzz/src/1st.spec.js
+++ fizzbuzz/src/1st.spec.js
@@ -1,3 +1,2 @@
-it('true is true', function () {
-    return expect(true).toEqual(true);
-});
+it('true is true', function () { return expect(true).toEqual(true); });
+//# sourceMappingURL=1st.spec.js.map
