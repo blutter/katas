131fd42e32d121a0e746dac1e12bfdcaf58b82e6
Benjamin Tse <blt@vsl.com.au>
2016-06-12 23:49:57 +1000

    Update template

--- ts-template/gulpfile.js
+++ ts-template/gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require("gulp");
+var jasmine = require('gulp-jasmine');
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var runSequence = require('run-sequence');
 
 gulp.task("default", function () {
     return tsProject.src()
         .pipe(ts(tsProject))
         .js.pipe(gulp.dest("dist"));
+});
+
+gulp.task('test', function() {
+   return gulp.src('src/*.spec.js')
+      .pipe(jasmine());
+});
+
+gulp.task('watch', function() {
+  runSequence('default');
+  gulp.watch('src/*.ts', ['default']);
 });
