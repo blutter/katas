b2e289db2a4d9a8ef73f35c884bd6e85f83a9651
Benjamin Tse <blt@vsl.com.au>
2016-06-14 23:10:39 +1000

    Tidy up Fix copying of html files to dist

--- ts-template/gulpfile.js
+++ ts-template/gulpfile.js
@@ -1,18 +1,25 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
 var runSequence = require('run-sequence');
 
-gulp.task("default", function () {
+var distPath = 'dist';
+
+gulp.task('default', ['copy-html'], function () {
     return gulp.src('src/*.ts')
         .pipe(ts(tsProject))
-        .js.pipe(gulp.dest("dist"));
+        .js.pipe(gulp.dest(distPath));
 });
 
 gulp.task('test', function() {
    return gulp.src('src/*.spec.js')
       .pipe(jasmine());
+});
+
+gulp.task('copy-html', function() {
+  return gulp.src('src/*.html')
+      .pipe(gulp.dest(distPath));
 });
 
 gulp.task('watch', function() {
